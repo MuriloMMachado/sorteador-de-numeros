8ec04ddcba67754571f901be2bd4eef3ce1fdf3a
Murilo Medeiros <murilomedeirosmachado12@gmail.com>
2025-02-10 19:48:42 -0300

    Função reiniciar

--- app.js
+++ app.js
@@ -48,4 +48,11 @@ function obterNumeroAleatorio(min, max) {
             botao.classList.add("container__botao-desabilitado");
         }
     }
+    function reiniciar() {
+        document.getElementById('quantidade').value = '';
+        document.getElementById('de').value = '';
+        document.getElementById('ate').value = '';
+        document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+        alterarStatusBotao();
+    }
 }
