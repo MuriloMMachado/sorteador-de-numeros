ac13ddf7b72ac99605640f048667bc5880772b47
Murilo Medeiros <murilomedeirosmachado12@gmail.com>
2025-02-10 19:42:01 -0300

    Adicionar validação da quantidade de números

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ function sortear() {
         alert("O campo \"Do número\" deve ser inferior ao \"Até o número\"");
         return;
     }
+    function sortear() {
+        let quantidade = parseInt(document.getElementById('quantidade').value);
+        let numInicial = parseInt(document.getElementById('de').value);
+        let numFinal = parseInt(document.getElementById('ate').value);
+        
+        if (numInicial >= numFinal) {
+            alert("O campo \"Do número\" deve ser inferior ao \"Até o número\"");
+            return;
+        }
+    
+        if (quantidade > (numFinal - numInicial + 1)) {
+            alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
+            return;
+        }
+    }
 }
